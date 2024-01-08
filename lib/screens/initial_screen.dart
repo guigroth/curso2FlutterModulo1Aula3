80f99b0ddb27be42329c9610e2497f765190acb9
Guilherme Augusto Groth <guilhermegroth@gmail.com>
2024-01-07 23:40:58 -0300

    primeiro commit

---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:nosso_primeiro_projeto/components/task.dart';
+import 'package:nosso_primeiro_projeto/screens/form_screen.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: const Text('Tarefas'),
+      ),
+      body: ListView(
+        children: const [
+          Task('Aprender Flutter', 'assets/images/dash.png', 3),
+          Task('Andar de Bike', 'assets/images/bike.webp', 2),
+          Task('Meditar', 'assets/images/meditar.jpeg', 5),
+          Task('Ler', 'assets/images/livro.jpg', 4),
+          Task(
+            'Jogar',
+            'assets/images/jogar.jpg',
+            1,
+          ),
+          SizedBox(
+            height: 80,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => FormScreen(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
